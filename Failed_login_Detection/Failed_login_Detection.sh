b4e7dc4632302a5389ff492f935d042c1bb9d93b
Usman Ghani <usman328ghani@gmail.com>
2026-03-04 20:22:21 +0500

    Updating the code

--- Failed_login_Detection/Failed_login_Detection.sh
+++ Failed_login_Detection/Failed_login_Detection.sh
@@ -2,10 +2,14 @@
 
 path='/var/log/auth.log'
 
-sudo grep "Failed Password" "$path" | awk '{print $(NF-3)}' | sort | nuniq -c | while read count ip
+echo "Checking Failed SSH login attempts"
+
+sudo grep "Failed password" "$path" | awk '{print $(NF-3)}' | sort | uniq -c | while read count ip
 do 
-	if [[ count > 5 ]];then
-		echo "Blocking "$ip" Attemptd "$count"
-		sudo iptables -A INPUT -s '$ip" -j DROP
+	if [[ "$count" -gt 5 ]];then
+		echo "Blocking "$ip" Attempts "$count"
+		sudo iptables -A INPUT -s "$ip" -j DROP
 	fi
 done
+
+echo "Done"
