9c972d79ccb72953390e38043b47acfee7e4a18a
Usman Ghani <usman328ghani@gmail.com>
2026-03-11 20:19:27 +0500

    Update Multi_Server_SSH_Automation.sh

--- Multi_Server_SSH_Automation/Multi_Server_SSH_Automation.sh
+++ Multi_Server_SSH_Automation/Multi_Server_SSH_Automation.sh
@@ -7,7 +7,9 @@ for server in "${servers[@]}";
 do
 	echo "Connecting to the $server: " 
 
-	result=$(ssh user@"$server" "uptime")
+	result=$(ssh user@"$server" "uptime" 2>/dev/null || echo "Connection Failed")
 
 	echo "$server : $result" >> report
 done
+
+echo "Report save in Report file"
